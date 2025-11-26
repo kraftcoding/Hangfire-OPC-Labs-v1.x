1fe7a8362eec2dd5816d891cc58566128db886bd
Kraftcoding <i.elg.alzaa@gmail.com>
2025-11-26 07:49:38 -0800

    Minor update

--- Hangfire.ConsoleApplication/Startup.cs
+++ Hangfire.ConsoleApplication/Startup.cs
@@ -33,7 +33,7 @@ namespace Hangfire.ConsoleApplication
 
             // Jobs
             BackgroundJob.Enqueue(() => ServerJob.Init(InitConstants.SERVER_JOB_FILE, filesPath));
-            BackgroundJob.Schedule(() => SuscribeNodesClientJob.Init(InitConstants.SUBSCRIBE_NODES_CLIENT_JOB_FILE, filesPath), TimeSpan.FromMinutes(1));  
+            BackgroundJob.Schedule(() => SuscribeNodesClientJob.Init(InitConstants.SUBSCRIBE_NODES_CLIENT_JOB_FILE, filesPath), TimeSpan.FromMinutes(1));
             RecurringJob.AddOrUpdate(InitConstants.NODE_WRITE_CLIENT_JOB_ID, () => NodeWriteClientJob.Init(InitConstants.NODE_WRITE_CLIENT_JOB_FILE, filesPath), InitConstants.CRON_MINUTELY);
             RecurringJob.AddOrUpdate(InitConstants.NODE_READ_CLIENT_JOB_ID, () => NodeReadClientJob.Init(InitConstants.NODE_READ_CLIENT_JOB_FILE, filesPath), InitConstants.CRON_MINUTELY);
         }
--- scripts/Get_NodeData.sql
+++ scripts/Get_NodeData.sql
@@ -1,3 +1,3 @@
 SELECT id, idProcess, processStartDate, DateTimeFieldName6, StringFieldName6, DoubleFieldName6
-  FROM [GenericUAServer].[dbo].[ReadingsData]
+  FROM [GenericOPCServer].[dbo].[ReadingsData]
   ORDER BY id DESC
